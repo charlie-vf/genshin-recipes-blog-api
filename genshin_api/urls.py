428fdf242960ce6fe3e7579f3c74f8f3f3982809
Charlie <charlottevfarmer@gmail.com>
2023-01-30 11:22:44 +0000

    Add create function to Likes serializer to handle duplicate likes on same recipe

--- genshin_api/urls.py
+++ genshin_api/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('profiles.urls')),
     path('', include('recipes.urls')),
     path('', include('comments.urls')),
+    # path('', include('likes.urls')),
 ]
--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Likes
 
@@ -14,3 +15,12 @@ class LikesSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'liked_at', 'owner', 'recipe'
         ]
+
+    # Handles duplicates through unique constraint declared in model
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'details': 'possible duplicate like'
+            })
