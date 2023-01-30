b32d39c7d45e262e7ba0db7fb8f872cca9dd4daa
Charlie <charlottevfarmer@gmail.com>
2023-01-30 12:26:45 +0000

    Refact profile and recipes views

--- profiles/views.py
+++ profiles/views.py
@@ -1,48 +1,21 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from genshin_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from genshin_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
-    """
-    List all profiles
-    """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    '''
+        List all profiles
+    '''
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
 
 
-class ProfileDetail(APIView):
-
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    '''
+        Retrieve or update a profile if you're the owner.
+    '''
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Profile.objects.all()
--- recipes/views.py
+++ recipes/views.py
@@ -1,79 +1,27 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from genshin_api.permissions import IsOwnerOrReadOnly
 from .models import Recipe
 from .serializers import RecipeSerializer
-from genshin_api.permissions import IsOwnerOrReadOnly
 
 
-class RecipeList(APIView):
-    """
-    List recipes & create if logged in
-    """
+class RecipeList(generics.ListAPIView):
+    '''
+        List recipes & create if logged in
+    '''
+
     serializer_class = RecipeSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Recipe.objects.all()
 
-    def get(self, request):
-        recipes = Recipe.objects.all()
-        serializer = RecipeSerializer(
-            recipes, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = RecipeSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class RecipeDetail(APIView):
-    """
-    Retrieve a recipe and edit or delete if you own it
-    """
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    '''
+        Retrieve a recipe and edit or delete if you own it
+    '''
+
+    serializer_class = RecipeSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = RecipeSerializer
-
-    def get_object(self, pk):
-        try:
-            recipe = Recipe.objects.get(pk=pk)
-            self.check_object_permissions(self.request, recipe)
-            return recipe
-        except Recipe.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(
-            recipe, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(
-            recipe, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        recipe = self.get_object(pk)
-        recipe.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Recipe.objects.all()
