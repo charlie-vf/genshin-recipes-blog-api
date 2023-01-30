b8d902f043e500b4b3f1ca1cb3e338111b88de60
Charlie <charlottevfarmer@gmail.com>
2023-01-30 13:19:38 +0000

    Annotate Profile views querysets to include count fields

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,6 +7,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    recipes_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -25,5 +28,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'is_owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'following_id',
+            'content', 'image', 'following_id', 'recipes_count',
+            'followers_count', 'following_count',
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from genshin_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -9,7 +10,26 @@ class ProfileList(generics.ListAPIView):
         List all profiles
     '''
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        recipes_count=Count('owner__recipe', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
+
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+
+    # sort profiles by number of recipes, followers, following
+    # sort profiles by date user followed profile & was followed
+    # by another user
+    ordering_fields = [
+        'recipes_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at'
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -18,4 +38,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     '''
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        recipes_count=Count('owner__recipe', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
