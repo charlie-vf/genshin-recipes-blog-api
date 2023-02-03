9d99115bd19493f76b47cc1488b43894db2e4a3a
Charlie <charlottevfarmer@gmail.com>
2023-02-03 09:26:46 +0000

    Re-add env.py as it deleted

--- genshin_api/settings.py
+++ genshin_api/settings.py
@@ -113,6 +113,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+if "CLIENT_ORIGIN" in os.environ:
+    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
+
+
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
