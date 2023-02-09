d766228d73b58ed9736c3243ffa17cf1f41745b1
Charlie <charlottevfarmer@gmail.com>
2023-02-09 10:32:20 +0000

    Remove previous fix attempt

--- recipes/views.py
+++ recipes/views.py
@@ -20,26 +20,6 @@ class RecipeList(generics.ListAPIView):
         made_count=Count('made', distinct=True),
     ).order_by('-created_at')
 
-    def get(self, request):
-        recipes = Recipe.objects.all()
-        serializer = RecipeSerializer(
-            recipes, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = RecipeSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
